5d102451aa0c8070afff8bb749386b3389898cdf
Sung Bae <singsang2@gmail.com>
2018-09-30 18:34:15 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -48,7 +48,7 @@ class TicTacToe
     @board[index].include?("X") || @board[index].include?("O")
   end
   
-  def valid_move?
+  def valid_move?(index)
     index.between?(0,8) && !position_taken?(index) 
   end
   
